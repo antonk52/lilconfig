17ea5d15d63332d9a84ba6782b38289421b4431c
antonk52 <halloy52@gmail.com>
2021-06-06 22:58:25 +0300

    bump dev dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:prettier/recommended',
-        'prettier/@typescript-eslint',
+        'prettier',
     ],
     plugins: ['@typescript-eslint', 'prettier'],
     parser: '@typescript-eslint/parser',
